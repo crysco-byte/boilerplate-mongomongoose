cb391ff2cd5a92bededff7ead0f91116e09603c6
cysco-byte <cryptoescor@protonmail.com>
2021-01-15 20:31:53 -0300

    added functionality

--- myApp.js
+++ myApp.js
@@ -90,8 +90,10 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, (err, result) => {
+    if (err) return console.log(err);
+    done(null, result);
+  });
 };
 
 const queryChain = (done) => {
