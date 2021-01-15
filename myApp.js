f99c4ac1cea6ce1f5bc106fe9c3bfc7eff22df91
cysco-byte <cryptoescor@protonmail.com>
2021-01-15 19:23:15 -0300

    added a functionality

--- myApp.js
+++ myApp.js
@@ -42,7 +42,10 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, (err, result) => {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const findPersonById = (personId, done) => {
